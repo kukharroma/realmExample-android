6375ebab2ea07bd1821dd848ac26f6ea51dd9446
Roman Kukhar <kukharroma@gmail.com>
2016-03-17 17:04:19 +0200

    [feature] added fabric

--- app/src/main/java/com/cook/simplerealmandroid/app/SimpleRealmApp.java
+++ app/src/main/java/com/cook/simplerealmandroid/app/SimpleRealmApp.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.cook.simplerealmandroid.realm.module.SimpleRealmModule;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -17,6 +19,7 @@ public class SimpleRealmApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         instance = this;
         RealmConfiguration config = new RealmConfiguration.Builder(getApplicationContext()).setModules(new SimpleRealmModule()).build();
         Realm.setDefaultConfiguration(config);
